7dacbed2085e6fb53e3a7a87b8b846dd8a753407
Eddie Madrigal <edmadrigal@yahoo.com>
2020-07-23 12:35:05 -0700

    single_number.py refactored

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,10 +3,11 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
-
-    pass
-
+    myNums = []
+    ans = arr[0]
+    for i in range(1, len(arr)):
+        ans ^= arr[i] # bitwise xor and assigns the value to ans. if xor is the same, 0 is returned        
+    return ans
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
