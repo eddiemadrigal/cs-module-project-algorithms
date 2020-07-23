08b68d2ceb19e4a3bb809db1482d63b107e7edae
Eddie Madrigal <edmadrigal@yahoo.com>
2020-07-23 13:05:06 -0700

    moving_zeroes.py refactored

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,9 +3,22 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
+    count = 0                               # count of non-zero elements
+    remainingZeroCount = 0                  # count of zero elements
+    updateArr = []                          # updated array of non-zero elements   
+    for el in range(len(arr)):              # iterate through 
+        if arr[el] != 0:                    # check each el if it's not a zero
+            count += 1                      # update non-zero count
+            updateArr.append(arr[el])       # append non-zero element
+        elif arr[el] == 0:                  # if a zero is found
+            remainingZeroCount += 1         # increase zero count
+    zeroCount = 0                           # initialize zero count to 0
+    while zeroCount < remainingZeroCount:   # while loop runs zeroCount times
+        updateArr.append(0)                 # for every zeroCount, append a zero
+        zeroCount += 1                      # increment the zeroCount by 1
+        
+    return updateArr                        # return the updated array
 
-    pass
 
 
 if __name__ == '__main__':
